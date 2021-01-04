10b309dd7b2b810eb2aa7c87c953d12db63158e0
jackmt9 <jackmt9@gmail.com>
2021-01-04 15:43:29 -0500

    component individual folders with css files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Main from './components/Main'
-import About from './components/About'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
+import Main from './components/Home/Home'
+import About from './components/About/About'
+import Projects from './components/Projects/Projects'
+import Contact from './components/Contact/Contact'
+import NavBar from './components/NavBar/NavBar'
+import {Switch, Route, withRouter} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <Main />
-      <About />
-      <Projects />
-      <Contact />
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <NavBar />
+      </div>
+    );
+}
 }
 
 export default App;
