90a23438c0bb5215b7b724377c3685e88e5cea85
jackmt9 <jackmt9@gmail.com>
2021-01-04 16:24:46 -0500

    navbar styling

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import Github from '../images/Octocat.png'
-import LinkedIn from '../images/linkedin.png'
-import Medium from '../images/medium.png'
-import Leetcode from '../images/leetcode.png'
-import Dev from '../images/dev.png'
-import Email from '../images/email.png'
+// import React from 'react';
+// import Github from '../images/Octocat.png'
+// import LinkedIn from '../images/linkedin.png'
+// import Medium from '../images/medium.png'
+// import Leetcode from '../images/leetcode.png'
+// import Dev from '../images/dev.png'
+// import Email from '../images/email.png'
 
-function Contact() {
-    return (
-        <div className="Contact">
-            <h2>Contact me here:</h2>
-            <p>contact info here</p>
-            <a href="https://github.com/Jackmt9" target="_blank"><img className="contact_button" src={Github} alt="GitHub"/></a>
-            <a href="https://www.linkedin.com/in/jackmt9/" target="_blank"><img className="contact_button" src={LinkedIn} alt="LinkedIn"/></a>
-            <a href="https://jackmt9.medium.com/" target="_blank"><img className="contact_button" src={Medium} alt="Medium"/></a>
-            <a href="https://dev.to/jackmt9" target="_blank"><img className="contact_button" src={Dev} alt="Dev"/></a>
-            <a href="https://leetcode.com/Jackmt9/" target="_blank"><img className="contact_button" src={Leetcode} alt="LeetCode"/></a>
-            <a href="mailto: jackmt9@gmail.com"><img className="contact_button" src={Email} alt="Email"/></a>
-        </div>
-    )
-}
+// function Contact() {
+//     return (
+//         <div className="Contact">
+//             <h2>Contact me here:</h2>
+//             <p>contact info here</p>
+//             <a href="https://github.com/Jackmt9" target="_blank"><img className="contact_button" src={Github} alt="GitHub"/></a>
+//             <a href="https://www.linkedin.com/in/jackmt9/" target="_blank"><img className="contact_button" src={LinkedIn} alt="LinkedIn"/></a>
+//             <a href="https://jackmt9.medium.com/" target="_blank"><img className="contact_button" src={Medium} alt="Medium"/></a>
+//             <a href="https://dev.to/jackmt9" target="_blank"><img className="contact_button" src={Dev} alt="Dev"/></a>
+//             <a href="https://leetcode.com/Jackmt9/" target="_blank"><img className="contact_button" src={Leetcode} alt="LeetCode"/></a>
+//             <a href="mailto: jackmt9@gmail.com"><img className="contact_button" src={Email} alt="Email"/></a>
+//         </div>
+//     )
+// }
 
-export default Contact
+// export default Contact
