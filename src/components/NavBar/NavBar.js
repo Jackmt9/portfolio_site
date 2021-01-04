11764d7e9ddd61b48d64fa75df059c83df251309
jackmt9 <jackmt9@gmail.com>
2021-01-04 16:10:48 -0500

    google fonts

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,14 +7,16 @@ export default class NavBar extends React.Component {
       <nav className="NavbarItems">
         <h1 className="navbar-logo">React</h1>
         <div className="menu-icon">
-
-
+          <i className="fab fa-react"></i>
         </div>
         <ul>
           {MenuItems.map((item, index) => {
             return (
-              <li><a className={MenuItems.cName}></a></li>
-
+              <li key={index}>
+                <a className={MenuItems.cName} href={item.url}>
+                {item.title}
+                </a>
+              </li>
             )
           })}
         </ul>
