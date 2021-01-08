7e750f2c5df06482c4ce374ad69214afd83809eb
jackmt9 <jackmt9@gmail.com>
2021-01-08 16:33:59 -0500

    footer content added with hyperlinks

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App(){
           </Router>
         </div>
 
-        <Footer className='footer'/>
+        <Footer />
       </div>
     );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,7 +9,13 @@ function Footer() {
     return (
         <footer>
             <div class='footnote'>
-                Jack Tawil <span class="highlight">©{year}</span>
+                <a href="https://jackmt9.medium.com/" target='_blank'><i class="fab fa-medium"></i></a>
+                <a href="https://www.linkedin.com/in/jackmt9/" target='_blank'><i class="fab fa-linkedin"></i></a>
+                <a href="https://github.com/jackmt9" target='_blank'><i class="fab fa-github-square"></i></a>
+                <a href="https://dev.to/jackmt9" target='_blank'><i class="fab fa-dev"></i></a>
+                <br/>
+                <p>Made With React, CSS, and ❤️</p>
+                <p>Jack Tawil <span class="highlight">©{year}</span></p>
             </div>
         </footer>
     )
