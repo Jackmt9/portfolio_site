7d84619ccf89384e6fdb3d828de33c875eb47c79
jackmt9 <jackmt9@gmail.com>
2021-01-24 21:13:05 -0500

    addded phone number to contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,10 +2,17 @@ import React from 'react';
 import './Contact.css'
 
 class Contact extends React.Component {
-    state={show: false}
+    state={
+        emailShow: false,
+        phoneShow: false
+    }
 
-    toggleShow = () => {
-        this.setState({show: !this.state.show})
+    toggleEmailShow = () => {
+        this.setState({emailShow: !this.state.emailShow})
+    }
+
+    togglePhoneShow = () => {
+        this.setState({phoneShow: !this.state.phoneShow})
     }
 
     render(){
@@ -19,12 +26,22 @@ class Contact extends React.Component {
                 <p>Thanks for checking out my site. Feel free to reach out to me via email to get in touch.</p>
                 <br/>
                 {
-                    !this.state.show ?
+                    !this.state.emailShow ?
                     <>
-                    <p>Email:</p><button onClick={this.toggleShow} className='show-button'><span>Click To Show</span></button>
+                    <p>Email:</p><button onClick={this.toggleEmailShow} className='show-button'><span>Click To Show</span></button>
                     </>
                     :
                     <p>Email: <a href='mailto:jackmt9@gmail.com' >jackmt9@gmail.com</a></p>
+                }
+                <br/>
+                {
+                    !this.state.phoneShow ?
+                    <>
+                    <p>Phone:</p>
+                    <button onClick={this.togglePhoneShow} className='show-button'><span>Click To Show</span></button>
+                    </>
+                    :
+                    <p>Mobile: <a href='tel://9177970898/' >(917) 797-0898</a></p>
                 }
                 <br/>
                 <br/>
