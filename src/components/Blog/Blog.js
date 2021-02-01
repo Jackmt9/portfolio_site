47c36346b5260e417ac64e9f29ca4b18779bec87
jackmt9 <jackmt9@gmail.com>
2021-02-01 14:42:49 -0500

    iframe fix

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -3,7 +3,7 @@ import './Blog.css'
 
 const Blog = () => {
     return ( 
-        <iframe src='https://jackmt9.medium.com/' title='Medium Blog'/>
+        <iframe src='https://jackmt9.medium.com/' title='Medium Blog' className='medium-iframe'/>
     );
 }
  
