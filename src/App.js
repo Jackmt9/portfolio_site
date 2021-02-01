d76e8f1e592b9476ca23b9a615a6562639d4244c
jackmt9 <jackmt9@gmail.com>
2021-02-01 14:38:14 -0500

    blog iframe instead of redirect

--- src/App.js
+++ src/App.js
@@ -7,17 +7,10 @@ import Resume from './components/Resume/Resume'
 import Contact from './components/Contact/Contact'
 import NavBar from './components/NavBar/NavBar'
 import Footer from './components/Footer/Footer'
+import Blog from './components/Blog/Blog'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App(){
-  function redirectToBlog(){
-  //   window.open(
-  //     "https://jackmt9.medium.com/"
-  //   )
-  //   window.location.href = '/'
-    window.location.href = 'https://jackmt9.medium.com/'
-    return null
-  }
 
   return (
     <div className="App">
@@ -30,7 +23,7 @@ function App(){
             <Route path='/about' component={About}/>
             <Route path='/contact' component={Contact}/>
             <Route path='/resume' component={Resume}/>
-            <Route path='/blog' render={redirectToBlog}/>
+            <Route path='/blog' component={Blog}/>
           </Switch>
         </Router>
       </div>
---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Blog.css'
+
+const Blog = () => {
+    return ( 
+        <iframe src='https://jackmt9.medium.com/' title='Medium Blog'/>
+    );
+}
+ 
+export default Blog;
