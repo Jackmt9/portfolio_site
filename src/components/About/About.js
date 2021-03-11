9f60d1fe9b43c256225b1ace172e012a8d30349f
jackmt9 <jackmt9@gmail.com>
2021-03-11 17:44:52 -0500

    who's - whose

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,7 @@ function About() {
         <div className="About">
             <h1>About</h1>
             <br/>
-            <p>I'm a Full-Stack Software Engineer whose always been passionate about learning how things work. In the past, I was a magician entertaining at corporate events. I hold a Bachelors of Science in Mathematics and am avidly looking to expand my knowledge.</p>
+            <p>I'm a Full-Stack Software Engineer who's always been passionate about learning how things work. In the past, I was a magician entertaining at corporate events. I hold a Bachelors of Science in Mathematics and am avidly looking to expand my knowledge.</p>
             <br/>
             <br/>
             <div className='about-image'/>
